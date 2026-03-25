aa641424fee2a5e54f83350badb8b680460236f5
subh <subh@example.com>
2026-03-25 17:51:20 +0530

    Accomodated changes for awww

--- scripts/wallpaper-picker.sh
+++ scripts/wallpaper-picker.sh
@@ -8,7 +8,7 @@ choices=$(find "$WALL_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname
         | rofi -dmenu -i -p "Pick wallpaper")
 
 [ -z "$choices" ] && exit
-swww img "$choices" \
+awww img "$choices" \
 	--transition-fps 60 \
 	--transition-type any
 
